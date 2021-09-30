765d498d48a77a17684302ba7184fea7a3c0c948
silvia-toncelli <59296911+silvia-toncelli@users.noreply.github.com>
2021-09-30 14:15:48 -0400

    Update MainProgram.java

--- Class9_30_21/MainProgram.java
+++ Class9_30_21/MainProgram.java
@@ -3,7 +3,10 @@ public class MainProgram
 {
    public static void main (String[] args) 
    {
-      Random rand= new Random();
-      System.out.println(rand.nextInt(44));
+      //Random rand= new Random();
+      //System.out.println(rand.nextInt(44));
+      
+      Animal cat = new Animal(5, 10);
+      System.out.println("The number of legs on the cat is: "+ cat.getNumOfLegs());
    }
 }
