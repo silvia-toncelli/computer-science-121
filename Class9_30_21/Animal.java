210334d771da6dea1302b504fdb1a51af427875a
silvia-toncelli <59296911+silvia-toncelli@users.noreply.github.com>
2021-09-30 14:11:36 -0400

    Add files via upload

---
+++ Class9_30_21/Animal.java
@@ -0,0 +1,35 @@
+public class Animal
+{
+   //attributes
+   private int numberOfLegs;
+   private double age;
+   private double height;
+   private double weight;
+   
+   //constructor
+   public Animal(double hgt, double wgt)
+   {
+      this.numberOfLegs = 4;
+      this.height = hgt;
+      this.weight = wgt;
+   }
+   
+   public Animal()
+   {
+      this.age = 10;
+   }
+   
+   //methods
+   public void setNumLegs(int legs) 
+   {
+      this.numberOfLegs=legs;
+   }
+   public double getAge()
+   {
+      return this.age;
+   }
+   public void printMessage()
+   {
+      System.out.println("Welcome to our zoo!");
+   }
+}
---
+++ Class9_30_21/MainProgram.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MainProgram
+{
+   public static void main (String[] args) 
+   {
+      Random rand= new Random();
+      System.out.println(rand.nextInt(44));
+   }
+}
